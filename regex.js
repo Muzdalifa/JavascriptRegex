d4891764155eb56bd0ad1fd0b2db221da6ddf28e
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2021-03-26 10:44:14 +0100

    match multiple string in string literals

--- regex.js
+++ regex.js
@@ -1,9 +1,10 @@
+//search literal string
 function findMatchString(){
 let x = document.getElementById("p1").innerHTML
 let y = document.getElementById("p2").innerHTML
-let myRegex = /I will./
+let myRegex = /I will./g
 document.getElementById("p3").innerHTML = `Matched string : ${myRegex}` ;
-//document.getElementById("p4").innerHTML = myRegex.test(y);
+//document.getElementById("p4").innerHTML = myRegex.test(y); this return t
 document.getElementById("p4").innerHTML = y.match(myRegex);
 
 }
