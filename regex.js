b58e259395e495585f807e1b83b33dab56ad943b
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2021-03-26 10:54:54 +0100

    ignore case when matching

--- regex.js
+++ regex.js
@@ -1,8 +1,7 @@
 //search literal string
 function findMatchString(){
-let x = document.getElementById("p1").innerHTML
 let y = document.getElementById("p2").innerHTML
-let myRegex = /I|will| home/g // g means global ie search all strings, here the space matter, if yo keep space you might get an error
+let myRegex = /I will|home/gi // g means global ie search all strings, here the space matter, if yo keep space you might get an error
 document.getElementById("p3").innerHTML = `Matched string : ${myRegex}` ;
 //document.getElementById("p4").innerHTML = myRegex.test(y); this return t
 document.getElementById("p4").innerHTML = y.match(myRegex);
