4af7f4b7ac5224b581f0e116d18ee972a38fd54f
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2021-03-26 12:04:10 +0100

    match numbers and single letters eg [2-6h-s]

--- regex.js
+++ regex.js
@@ -32,10 +32,18 @@ console.log(y.match(myRegex)) //this return array of all matched string
 
 function matchAtoZ(){
  let y = document.getElementById("p4a").innerHTML
-let myRegex = /[a-z]/gi // return ba or be or bi or bo or bu,
-//let myRegex = /b[aeiou]g/gi // the first letter must be b and the last must be g, in between can have vowel a,e,i,o,u
+let myRegex = /[a-z]/gi 
 document.getElementById("p4b").innerHTML = `Matched string : ${myRegex}`;
 document.getElementById("p4c").innerHTML = y.match(myRegex);
-console.log(y.match(myRegex)) //this return array of all matched string
+console.log(y.match(myRegex)) //this return array of all matched character
+ 
+}
+
+function matchLettersAndNumbers(){
+  let y = document.getElementById("p5a").innerHTML
+let myRegex = /[2-6h-s]/gi //this match single character 
+document.getElementById("p5b").innerHTML = `Matched string : ${myRegex}`;
+document.getElementById("p5c").innerHTML = y.match(myRegex);
+console.log(y.match(myRegex)) //this return array of all matched character
  
 }
