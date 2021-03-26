68304443990317a2801f4a78281d6d772e19d404
Muzdalifa Ali <a.muzdalifa27@gmail.com>
2021-03-26 11:15:07 +0100

    search  string with wilcard .

--- regex.js
+++ regex.js
@@ -5,7 +5,18 @@ let myRegex = /I will|home/gi // g means global ie search all strings, here the 
 document.getElementById("p3").innerHTML = `Matched string : ${myRegex}` ;
 //document.getElementById("p4").innerHTML = myRegex.test(y); this return t
 document.getElementById("p4").innerHTML = y.match(myRegex);
+console.log(y.match(myRegex)) //this return array of all matched string
 
+}
+
+//search  string with wilcard .
+function findStringWithPeriod(){
+let y = document.getElementById("p2a").innerHTML
+let myRegex = /hu./gi // match any word starting with hu
+//let myRegex = /.hu/gi // match any word ending with hu
+document.getElementById("p2c").innerHTML = `Matched string : ${myRegex}`;
+document.getElementById("p2d").innerHTML = y.match(myRegex);
+console.log(y.match(myRegex)) //this return array of all matched string
 
 }
  
